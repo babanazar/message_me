1548634853f9f6755acb8307f710e0eca21ce257
babanazar <g.babanazar@gmail.com>
2020-04-25 12:14:50 +0300

    added messages to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Evgeny", password: "password")
+User.create(username: "Jonsnow", password: "password")
+User.create(username: "Arya", password: "password")
+User.create(username: "Frodo", password: "password")
+User.create(username: "Gandalf", password: "password")
+
+Message.create(body: "Hello everyone", user: User.find(1))
+Message.create(body: "Hey new guy", user: User.find(6))
+Message.create(body: "I'm chillin lik", user: User.find(4))
